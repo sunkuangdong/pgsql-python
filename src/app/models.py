83d3e811294049a88a1c787677d4d88ff54cb956
sunkuangdong <769296817@qq.com>
2026-09-11 13:47:16 -0700

    Database migration

---
+++ src/app/models.py
@@ -0,0 +1,10 @@
+from app.users.model import User
+from app.conversations.model import Conversation
+from app.messages.model import Message
+
+
+__all__ = [
+    "User",
+    "Conversation",
+    "Message",
+]
